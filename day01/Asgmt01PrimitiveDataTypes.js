3c969f3886ca884a813307a6407c4a27ff5b390e
ChristyAmish <christynzt@gmail.com>
2026-07-22 15:43:23 +0530

    Add files via upload
    Classroom Assignment: Primitive Data Types 
     
    Learning Objective:  
    Learn primitive data types in JavaScript 
     
    Expected Completion Time:  
    Best Case: 10 minutes  
    Average Case: 15 minutes  
     
    Assignment Details:  
    Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, 
    including an uninitialized variable. 
     
    Assignment Requirements:  
        Create the following variables using let (not using var) and check their typeOf 
        a) firstName 
        b) companyName 
        c) mobileNumber 
        d) isAutomation 
        e) hasPlaywright (do not assign) 
     Print and confirm the values and data types 
     
    Hints to Solve:  
    Focus on initializing variables with different values, including a string, number, boolean, and leave one 
    variable undefined to practice with `typeof`. 
     
    Expected Outcome:  
    Upon completion, you should be able to:  - Grasp the concepts of different data types in JavaScript

---
+++ day01/Asgmt01PrimitiveDataTypes.js
@@ -0,0 +1,29 @@
+//Assignment Requirements:  
+    //Create the following variables using let (not using var) and check their typeOf 
+   // a) firstName 
+   // b) companyName 
+   // c) mobileNumber 
+   // d) isAutomation 
+   // e) hasPlaywright (do not assign) 
+ //Print and confirm the values and data types
+
+
+ let firstName=`EmployeenameA`;
+ console.log('First Name :' +firstName);
+ console.log("Type of first name is " +typeof(firstName));
+ let companyName = "Epam Systems";
+console.log('Company Name :' +companyName);
+ console.log("Type of company name is " +typeof(companyName));
+ let mobileNumber=9999999999;
+ console.log('Mobile Number :' +mobileNumber);
+  console.log("Type of mobile number is " +typeof(mobileNumber));
+   let isAutomation = true;
+ console.log('Automation skills :' +isAutomation);
+  console.log("Type of automation skills is " +typeof(isAutomation));
+  let hasPlaywright;
+ console.log('Playwright :' +hasPlaywright);
+  console.log("Type of playwright is " +typeof(hasPlaywright));
+
+
+
+
